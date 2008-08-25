1b41e382839ec9318a109d6e1a6ba11941f6c22a
George Armah <armahg@macports.org>
2008-08-25 05:08:32 +0000

    Added MPHelperToolIPCTester.m file and corresponding target for additional testing of Framework<->HelperTool IPC
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc08-framework@39566 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ MacPorts_Framework_Release/MPHelperToolIPCTester.m
@@ -0,0 +1,116 @@
+//
+//  MPHelperToolIPCTester.m
+//  MacPorts.Framework
+//
+//  Created by George  Armah on 8/24/08.
+//  Copyright 2008 Lafayette College. All rights reserved.
+//
+
+
+
+#import <Foundation/Foundation.h>
+#import <MacPorts/MacPorts.h>
+
+
+
+@interface PortManipulator : NSObject {
+	
+}
+-(BOOL) installUninstallManipulation:(NSString *)portName;
+@end
+
+@implementation PortManipulator
+-(BOOL) installUninstallManipulation:(NSString *)portName {
+	BOOL ret = NO;
+	
+	MPRegistry * registry = [MPRegistry sharedRegistry];
+	MPMacPorts * port = [MPMacPorts sharedInstance];
+	
+	// Check if portName is installed
+	unsigned int installed = [[registry installed:portName] count];
+	
+	// Search for it
+	NSDictionary * searchResult = [port search:portName];
+	NSArray * keyArray = [searchResult allKeys];
+	MPPort * foundPort = [searchResult objectForKey:[keyArray objectAtIndex:0]];
+	
+	// If it is installed
+	if (installed > 0) {
+		NSError * uError;
+		//Attempt to uninstall it
+		[foundPort uninstallWithVersion:nil error:&uError];
+		
+		//Check for error
+		if (uError != nil) {
+			NSLog(@"\n\nUninstallation of %@ failed with error %@", portName, uError);
+			//I guess we should just return here
+			return ret;
+		}
+		
+		//Uninstallation was successful ... now check registry to make sure its gone
+		installed = [[registry installed:portName] count];
+		if (installed > 0) { //Uh oh ... is this suppose to happen?
+			NSLog(@"%@ is still installed after successful uninstall operation ... double check this from commandline", portName);
+			//for now return
+			return ret;
+		}
+		else { // For now end here later on ... add more code to restore system to its original state ... hmm i could just
+			// call this method twice
+			ret = YES;
+			return ret;
+		}
+		
+	}
+	else {
+		NSError * uError;
+		//Attempt to install it
+		[foundPort installWithOptions:nil variants:nil error:&uError];
+		
+		//Check for error
+		if (uError != nil) {
+			NSLog(@"\n\nInstallation of %@ failed with error %@", portName, uError);
+			//I guess we should just return here
+			return ret;
+		}
+		
+		//Installation was successful ... now check registry to make sure its gone
+		installed = [[registry installed:portName] count];
+		if (installed == 0) { //Uh oh ... is this suppose to happen?
+			NSLog(@"%@ is not installed after successful install operation ... double check this from commandline", portName);
+			//for now return
+			return ret;
+		}
+		else { // For now end here later on ... add more code to restore system to its original state ... hmm i could just
+			// call this method twice
+			ret = YES;
+			return ret;
+		}
+	}
+	NSLog(@"We shouldn't be here");
+	return YES;
+}
+@end
+
+int main(int argc, char const * argv[]) {
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+	
+	
+	PortManipulator * pm = [[PortManipulator alloc] init];
+	
+	if([pm installUninstallManipulation:@"pngcrush"]) {
+		NSLog(@"pngcrush INSTALLATION SUCCESSFUL");
+	}
+	else {
+		NSLog(@"pngcrush INSTALLATION UNSUCCESSFUL");
+	}
+	
+	
+	[pm release];
+	
+	
+	
+	[pool release];
+	
+	return 0;
+}
+
