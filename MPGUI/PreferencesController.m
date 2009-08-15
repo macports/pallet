0ef86eda0c1e8d702e8436514939d8964c76403c
Juan Germán Castañeda Echevarría <juanger@macports.org>
2009-08-15 01:19:12 +0000

    Validation for macports path in Preferences panel.
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc09-gui@55629 d073be05-634f-4543-b044-5fe20cf6d1d6

--- MPGUI/PreferencesController.m
+++ MPGUI/PreferencesController.m
@@ -19,11 +19,27 @@
     NSInteger result = [openPanel runModalForDirectory:[defaults objectForKey:@"NSNavLastCurrentDirectory"] file:nil types:nil];
     if(result == NSOKButton) {
         NSString *path = [[openPanel filenames] objectAtIndex:0];
-        [defaults setObject:path forKey:@"PKGPath"];
-        NSLog(@"PATH: %@", path);
-        [MPMacPorts setPKGPath:path];
-        [[MPActionLauncher sharedInstance] 
-                    performSelectorInBackground:@selector(loadPorts) withObject:nil];
+        // Validate Directory
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        BOOL existsAsDirectory;
+        NSString *macportsDir = [path stringByAppendingPathComponent:@"macports1.0"];
+        NSString *macportsFile = [macportsDir stringByAppendingPathComponent:@"macports.tcl"];
+        BOOL containsMacPortsTcl;
+        [fileManager fileExistsAtPath:macportsDir isDirectory:&existsAsDirectory];
+        containsMacPortsTcl = [fileManager fileExistsAtPath:macportsFile isDirectory:nil];
+        if(existsAsDirectory && containsMacPortsTcl) {
+            [defaults setObject:path forKey:@"PKGPath"];
+            [MPMacPorts setPKGPath:path];
+            [[MPActionLauncher sharedInstance] 
+             performSelectorInBackground:@selector(loadPorts) withObject:nil];
+        } else {
+            NSAlert *alert = [NSAlert alertWithMessageText:@"You selected an invalid directory" 
+                                    defaultButton:@"OK" 
+                                    alternateButton:nil 
+                                    otherButton:nil 
+                                    informativeTextWithFormat:@"You have to use the directory where macports1.0 was installed by MacPorts.\n\nGenerally it should be /Library/Tcl"];
+            [alert runModal];
+        }
     }
 }
 
