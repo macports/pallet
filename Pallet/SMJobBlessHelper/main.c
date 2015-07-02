b94c3ca9312547683378fadb5de6a4ff8ba119bd
Kyle Sammons <ksammons@macports.org>
2015-07-02 17:42:40 +0000

    Removed the Growl framework files from the repo.
    git-svn-id: https://svn.macports.org/repository/macports/branches/gsoc15-pallet@138228 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ Pallet/SMJobBlessHelper/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  SMJobBlessHelper
+//
+//  Created by Mr. Appleseed on 7/2/15.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
